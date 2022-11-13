b5645dfb91d13a68a2ea2d5619e44a304118ddd1
EJDC <user@users-MacBook-Pro.local>
2022-11-13 11:57:55 +0000

    :hammer: Customers Controller v1

--- controllers/customers_controller.py
+++ controllers/customers_controller.py
@@ -10,3 +10,64 @@ customers_blueprint = Blueprint("customers", __name__)
 def customers():
     customers = customer_repository.select_all()
     return render_template("customers/index.html", customers=customers)
+
+# NEW
+@customers_blueprint.route("/customers/new")
+def new_customer():
+    return render_template("customers/new.html")
+
+# CREATE
+@customers_blueprint.route("/customers", methods=["POST"])
+def create_customer():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    dob = request.form["dob"]
+    email = request.form["email"]
+    membership_level = request.form["membership_level"]
+    membership_status = request.form["membership_status"]
+    payment_method = request.form["payment_method"]
+    extra_physio = request.form["extra_physio"]
+    extra_pt = request.form["extra_pt"]
+    extra_service_3 = request.form["extra_service_3"]
+    extra_service_4 = request.form["extra_service_4"]
+    # missed_classes = missed_classes
+    # monthly_bill = monthly_bill
+    # id = id
+    new_customer = Customer(first_name, last_name, dob, email, membership_level, membership_status, payment_method, extra_physio, extra_pt, extra_service_3, extra_service_4,)
+    customer_repository.save(new_customer)
+    return redirect("/customers")
+
+# EDIT
+@customers_blueprint.route("/customers/<id>/edit")
+def edit_customer(id):
+    customer = customer_repository.select(id)
+    return render_template('customers/edit.html', customer=customer)
+
+
+# UPDATE
+@customers_blueprint.route("/customers/<id>", methods=["POST"])
+def update_customer(id):
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    dob = request.form["dob"]
+    email = request.form["email"]
+    membership_level = request.form["membership_level"]
+    membership_status = request.form["membership_status"]
+    payment_method = request.form["payment_method"]
+    extra_physio = request.form["extra_physio"]
+    extra_pt = request.form["extra_pt"]
+    extra_service_3 = request.form["extra_service_3"]
+    extra_service_4 = request.form["extra_service_4"]
+    # missed_classes = missed_classes
+    # monthly_bill = monthly_bill
+    # id = id
+    customer = Customer(first_name, last_name, dob, email, membership_level, membership_status, payment_method, extra_physio, extra_pt, extra_service_3, extra_service_4,)
+    customer_repository.update(customer)
+    return redirect("/customers")
+
+
+# DELETE
+@customers_blueprint.route("/customers/<id>/delete", methods=["POST"])
+def delete_customer(id):
+    customer_repository.delete(id)
+    return redirect("/customers")
