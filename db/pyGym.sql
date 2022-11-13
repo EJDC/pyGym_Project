2173eca0458cde9b1444cf8726b7dd348cbb664e
EJDC <user@users-MacBook-Pro.local>
2022-11-13 18:05:12 +0000

    :card_file_box: pyGym SQL v1.2 - first working version + some unit tests on customer.py

--- db/pyGym.sql
+++ db/pyGym.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS sessions;
-DROP TABLE IF EXISTS staff;
-DROP TABLE IF EXISTS rooms;
-DROP TABLE IF EXISTS bookings;
-DROP TABLE IF EXISTS session_types;
-DROP TABLE IF EXISTS rooms_types
-DROP TABLE IF EXISTS staff_session_types
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS sessions CASCADE;
+DROP TABLE IF EXISTS staff CASCADE;
+DROP TABLE IF EXISTS rooms CASCADE;
+DROP TABLE IF EXISTS bookings CASCADE;
+DROP TABLE IF EXISTS session_types CASCADE;
+DROP TABLE IF EXISTS room_session_types CASCADE;
+DROP TABLE IF EXISTS staff_session_types CASCADE;
 
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
@@ -21,33 +21,17 @@ CREATE TABLE customers (
     extra_service_3 BOOLEAN,
     extra_service_4 BOOLEAN,
     missed_classes INT,
-    monthly_bill FLOAT,
+    monthly_bill FLOAT
 );
 
 CREATE TABLE session_types (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255)
 );
 
 CREATE TABLE rooms (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
-);
-
-CREATE TABLE sessions (
-    id SERIAL PRIMARY KEY,
-    name NAME,
-    date_and_time DATETIME,
-    duration INT,
-    min_age INT,
-    max_age INT,
-    p_member_price FLOAT,
-    s_member_price FLOAT,
-    max_capacity INT,
-    instructor_id INT NOT NULL REFERENCES staff(id),
-    instructor_payment FLOAT,
-    room INT NOT NULL REFERENCES rooms(id)
-    session_type INT NOT NULL REFERENCES session_types(id)
 );
 
 CREATE TABLE staff (
@@ -57,6 +41,23 @@ CREATE TABLE staff (
     email VARCHAR(255),
     monthly_invoice FLOAT
 );
+
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_and_time TIMESTAMP,
+    duration INT,
+    min_age INT,
+    max_age INT,
+    p_member_price FLOAT,
+    s_member_price FLOAT,
+    max_capacity INT,
+    instructor_id INT NOT NULL REFERENCES staff(id),
+    instructor_payment FLOAT,
+    room INT NOT NULL REFERENCES rooms(id),
+    session_type INT NOT NULL REFERENCES session_types(id)
+);
+
 
 CREATE TABLE bookings (
     id SERIAL PRIMARY KEY,
@@ -74,4 +75,4 @@ CREATE TABLE staff_session_types (
     id SERIAL PRIMARY KEY,
     staff_id SERIAL NOT NULL REFERENCES staff(id),
     session_type_id SERIAL NOT NULL REFERENCES session_types(id)
-)
+);
